acb1ef55a6b38ccb862b974546fdfed822f7e372
Shanu <smahanama@gmail.com>
2017-02-13 23:08:00 +0000

    RBG sliders show result in view box Additional view controller added Navigation between view controllers added

--- RGB Sliders/ViewController.swift
+++ RGB Sliders/ViewController.swift
@@ -10,11 +10,31 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var redSlider: UISlider!
+    
+    @IBOutlet var greenSlider: UISlider!
+    
+    @IBOutlet var blueSlider: UISlider!
+    
+    @IBOutlet var colorSquare: UIView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        updateBackgroundColor()
+        colorSquare.layer.borderColor = UIColor.black.cgColor
+        colorSquare.layer.borderWidth = 1
     }
 
+    @IBAction func updateBackgroundColor() {
+        let red = CGFloat(redSlider.value)
+        let green = CGFloat(greenSlider.value)
+        let blue = CGFloat(blueSlider.value)
+        colorSquare.backgroundColor = UIColor(red: red, green: green, blue: blue, alpha: 1)
+    }
+    
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
